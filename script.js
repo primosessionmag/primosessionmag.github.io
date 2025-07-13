b35d7d80d83ae610a4bf749818d7d4d4e4510613
joshlavroff <joshlavroff@gmail.com>
2025-07-13 17:15:53 -0400

    remove redundant comments

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ for (var i = 0; i < accordions.length; i++) {
 
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
-      // accordion is currently open, so close it
+      
       content.style.maxHeight = null;
     } else {
-      // accordion is currently closed, so open it
+    
       content.style.maxHeight = content.scrollHeight + "px";
     }
   }
